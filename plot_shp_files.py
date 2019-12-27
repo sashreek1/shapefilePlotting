2f32bd476889724df75f4530854a469813e34b03
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-27 19:48:58 +0530

    Update plot_shp_files.py

--- plot_shp_files.py
+++ plot_shp_files.py
@@ -5,7 +5,7 @@ fig_size[0] = 10
 fig_size[1] = 8
 plt.rcParams["figure.figsize"] = fig_size
 
-cities = ['Tokyo','Chennai', 'Delhi','Seoul','Bengaluru']
+cities = ['Bengaluru','Tokyo','Chennai', 'Delhi','Seoul']
 files = ['0','buildings.shp','landuse.shp','natural.shp','places.shp','railways.shp','roads.shp','waterways.shp']
 
 for city in cities:
@@ -13,17 +13,17 @@ for city in cities:
     print("What would you like to plot for",city)
     print('\n')
     print("""Choose the shp files that you would like to plot:(separate the numbers with spaces)
-1)buildings
-2)landuse
-3)natural
-4)places
-5)railways
-6)roads
-7)waterways
+1)Buildings
+2)Land use
+3)Natural
+4)Places
+5)Railways
+6)Roads
+7)Waterways
             """)
     print("\n")
     lst = list(map(int,input().split()))
-    fp = "shapefilesV2/"+city+"/shape/"+files[int(lst[0])]
+    fp = "shapefilesV3/"+city+"/shape/"+files[int(lst[0])]
     data = gpd.read_file(fp)
     ax = data.plot()
     for i in range(1,len(lst)) :
